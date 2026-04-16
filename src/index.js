4d161660fc33aee87a2b2ca14a3599592fe79bc9
OCookies2011 <ocookies2011@gmail.com>
2026-04-16 23:54:30 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Central re-export hub — imported by pages as "../index"
 
-export { default as AdminPanel }          from "./AdminPanel";
-export { StaffPage }                      from "./AdminStaff";
-export { ContactPage }                    from "./AdminContact";
-export { AboutPage, PlayerWaitlist, TermsPage } from "./PublicPages";
+export { default as AdminPanel }                from "./AdminPanel";
+export { StaffPage }                            from "./AdminStaff";
+export { ContactPage }                          from "./AdminContact";
+export { AboutPage, PlayerWaitlist, TermsPage } from "./pages/PublicPages";
